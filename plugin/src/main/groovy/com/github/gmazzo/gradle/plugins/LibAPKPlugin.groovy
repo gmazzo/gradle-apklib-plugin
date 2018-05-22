890e981997b82076b63d6af51f5a02ac1ef89e04
Guillermo Mazzola <gmazzo65@gmail.com>
2018-05-22 12:58:05 -0400

    Initial commit

---
+++ plugin/src/main/groovy/com/github/gmazzo/gradle/plugins/LibAPKPlugin.groovy
@@ -0,0 +1,13 @@
+package com.github.gmazzo.gradle.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LibAPKPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
